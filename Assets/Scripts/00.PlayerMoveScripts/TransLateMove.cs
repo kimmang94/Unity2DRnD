993f3cfcdd99eadb3029168c0e8cd165745687f9
kimhyeok <kimmang940715@naver.com>
2023-07-17 01:51:08 +0900

    Translate 를 Update에서 사용한 경우 버튼 클릭당 그리드 한칸씩 이동

--- Assets/Scripts/00.PlayerMoveScripts/TransLateMove.cs
+++ Assets/Scripts/00.PlayerMoveScripts/TransLateMove.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TransLateMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     private void Update()
+     {
+          TranslateMove();
+     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     private void TranslateMove()
+     {
+          if (Input.GetKeyDown(KeyCode.W))
+          {
+               Debug.Log("W Clicked");
+               gameObject.transform.Translate(0, 1, 0);
+          }
+          if (Input.GetKeyDown(KeyCode.S))
+          {
+               Debug.Log("S Clicked");
+               gameObject.transform.Translate(0, -1, 0);
+          }
+          if (Input.GetKeyDown(KeyCode.A))
+          {
+               Debug.Log("A Clicked");
+               gameObject.transform.Translate(-1, 0, 0);
+          }
+          if (Input.GetKeyDown(KeyCode.D))
+          {
+               Debug.Log("D Clicked");
+               gameObject.transform.Translate(1, 0, 0);
+          }
+     }
 }
