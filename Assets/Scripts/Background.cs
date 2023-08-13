3130c578484ca7b6fb0a0a79a02bc6365a5a5945
kimhyeok <kimmang940715@naver.com>
2023-08-14 08:52:20 +0900

    백그라운드 이미지가 일정위치 이하이면 위로 다시 올라가는 기능

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     private float moveSpeed = 3.0f;
+
+    private float resetPosition = 20.0f;
+    private float underPosition = -10.0f;
     
     private void Update()
     {
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < underPosition)
+        {
+            transform.position += new Vector3(0, resetPosition, 0);
+        }
     }
 }
