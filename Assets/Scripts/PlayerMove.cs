cc8f7455e86e076452eb9e24f84b4e4b8b869571
kimhyeok <kimmang940715@naver.com>
2023-08-16 05:22:43 +0900

    마우스 이동 최솟값 최댓값 설정

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -50,6 +50,7 @@ public class PlayerMove : MonoBehaviour
     {
         // 마우스 해상도 기준 -> 포지션위치로 변경
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+        float toX = Mathf.Clamp(mousePos.x, -2.35f, 2.35f);
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z);
     }
 }
