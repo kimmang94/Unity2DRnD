8faa94397788dc57793012a71d49fdd927a9bf49
kimhyeok <kimmang940715@naver.com>
2023-08-16 06:35:59 +0900

    Weapon Vector3.up 을 통한 위로 올라가는 기능 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float moveSpeed = 10f;
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
     }
 }
