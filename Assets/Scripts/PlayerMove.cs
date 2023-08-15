a4ac8feeee75ba657c0fd842d8b03d06a06171c1
kimhyeok <kimmang940715@naver.com>
2023-08-16 03:16:02 +0900

    좌우로만 움직이기 위한 주석 처리 및 스크립트 설명 추가

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    // 키보드 움직임 제어 스크립트
     [SerializeField]
     private float moveSpeed = 0;
     
     private void Update()
     { 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        // verticalInput = Input.GetAxisRaw("Vertical");
 
-        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
+        Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
 
         transform.position += moveTo * moveSpeed * Time.deltaTime;
         Debug.Log(horizontalInput);
-        Debug.Log(verticalInput);
+        //Debug.Log(verticalInput);
     }
 }
