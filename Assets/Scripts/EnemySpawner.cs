7da719ced6a236e6f6ae583fc61b01cc42d58d8d
kimhyeok <kimmang940715@naver.com>
2023-08-17 02:13:05 +0900

    적생성 코루틴 및 설명주석 추가

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,16 +8,42 @@ public class EnemySpawner : MonoBehaviour
     private GameObject[] enemies = null;
     
     private float[] arrPosX = { -2.2f, -1.1f, 0f, 1.1f, 2.2f };
+
+    [SerializeField]
+    private float spwanInterval = 1.5f;
     private void Start()
     {
-        foreach (float posX in arrPosX)
-        {
-            int index = Random.Range(0,enemies.Length);
-            SpawnEnemy(posX, index);
-        }
-        
+        StartEnemyRoutine();
     }
 
+    private void StartEnemyRoutine()
+    {
+        StartCoroutine("EnemyRoutine");
+    }
+    /// <summary>
+    /// 적 생성을 위한 코루틴
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator EnemyRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            foreach (float posX in arrPosX)
+            {
+                int index = Random.Range(0,enemies.Length);
+                SpawnEnemy(posX, index);
+            }
+
+            yield return new WaitForSeconds(spwanInterval);
+        }
+    }
+    /// <summary>
+    /// 생성 될 Pos X와 적 몬스터 배열 index를 받아 생성 
+    /// </summary>
+    /// <param name="posX"></param>
+    /// <param name="index"></param>
     private void SpawnEnemy(float posX, int index)
     {
         Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
